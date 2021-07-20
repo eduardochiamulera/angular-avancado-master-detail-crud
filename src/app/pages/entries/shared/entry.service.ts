87153cd8d6313be0b8edbb79d000a5ca99e92119
Eduardo Chiamulera <eduardo.chiamulera@totvs.com.br>
2021-07-20 18:30:03 -0300

    adding association between Entry and Category

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -5,6 +5,8 @@ import { map, catchError, flatMap } from "rxjs/operators";
 
 import { Entry } from "./entry.model";
 
+import { CategoryService } from "../../categories/shared/category.service";
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,7 +14,7 @@ export class EntryService {
 
   private apiPath: string = "api/entries";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private categoryService: CategoryService) { }
 
   getAll(): Observable<Entry[]>{
     return this.http.get(this.apiPath).pipe(
@@ -29,16 +31,36 @@ export class EntryService {
   }
 
   create(entry: Entry) : Observable<Entry> {
-    return this.http.post(this.apiPath, entry).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry))
+    //entry.categoryId = 1 => moradia
+    //entry.category = category // null
+
+    //map retornaria Observable<Observable<Entry>>
+    //flashMap retorna o Observable<Entry> resolvido
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+
+        //retorna um Observable<Entry>
+        return this.http.post(this.apiPath, entry).pipe(
+          catchError(this.handleError),
+          map(this.jsonDataToEntry))
+      })
+    )
+
+   
   }
 
   update(entry: Entry) : Observable<Entry> {
     const url = `${this.apiPath}/${entry.id}`;
-    return this.http.put(url, entry).pipe(
-      catchError(this.handleError),
-      map(() => entry));
+
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+        return this.http.put(url, entry).pipe(
+          catchError(this.handleError),
+          map(() => entry))
+      })
+    )
   }
 
   delete(id : number) : Observable<any>{
